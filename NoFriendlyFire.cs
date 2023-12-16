19e1cb93ea409537a8839ea18057ec9a063c8eac
Blooym <19539165+Blooym@users.noreply.github.com>
2023-12-16 23:29:52 +0000

    chore: initial commit

---
+++ NoFriendlyFire.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NoFriendlyFire
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class NoFriendlyFire : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+        public static ManualLogSource LoggerInstance { get; private set; }
+        
+        private void Awake()
+        {
+            LoggerInstance = this.Logger;
+            LoggerInstance.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} loaded successfully.");
+            _harmony.PatchAll();
+        }
+    }
+}
---
+++ Patches/NoFriendlyFireDamagePatch.cs
@@ -0,0 +1,18 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace NoFriendlyFire.Patches
+{
+    [HarmonyPatch]
+    internal class NoFriendlyFireDamagePatch
+    {
+        [HarmonyPatch(typeof(PlayerControllerB), "DamagePlayerFromOtherClientServerRpc")]
+        [HarmonyPrefix]
+        public static void DamagePlayerFromOtherClientServerRpc(ref int damageAmount, Vector3 hitDirection, int playerWhoHit)
+        {
+            NoFriendlyFire.LoggerInstance.LogDebug($"Player{playerWhoHit} attempted to friendly fire, setting damage amount to 0.");
+            damageAmount = 0;
+        }
+    }
+}
